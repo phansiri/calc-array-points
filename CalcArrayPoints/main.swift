7f4bfda88139e776099d9ca4c4e6d6132fa186eb
lit_phansiri <phansiri@uw.edu>
2016-10-08 15:12:44 -0700

    initial commit

--- CalcArrayPoints/main.swift
+++ CalcArrayPoints/main.swift
@@ -8,5 +8,42 @@
 
 import Foundation
 
-print("Hello, World!")
+// Add function
+func add(firstNumber x: Int, secondNumber y: Int) -> Int {
+    return x + y
+}
+
+// Subtract function
+func subtract(firstNumber x: Int, secondNumber y: Int) -> Int {
+        return x - y
+}
+
+// Multiply function
+func multiply(firstNumber x: Int, secondNumber y: Int) -> Int {
+    return x * y
+}
+
+// Divide function
+// BIG BUG FOR DIVIDING
+func divide(firstNumber x: Int, secondNumber y: Int) -> Int {
+        return x / y
+}
+
+//func divide(firstNumber x: Int, secondNumber y: Int) -> Double {
+//    return Double(x) / Double(y)
+//}
+
+// Basic math function
+func basicMath(firstNumber x: Int, secondNumber y: Int, operand: (Int, Int) -> Int) -> Int {
+    return operand(x, y)
+}
+
+print(basicMath(firstNumber: 10, secondNumber: 100, operand: divide))
+
+
+
+
+
+
+
 
