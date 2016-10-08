e4ded410c7d588b76b44d6ca6f83b3601ceaf585
lit_phansiri <phansiri@uw.edu>
2016-10-08 15:34:06 -0700

    added add and multiply function that takes in an array

--- CalcArrayPoints/main.swift
+++ CalcArrayPoints/main.swift
@@ -9,22 +9,22 @@
 import Foundation
 
 // Calculator
-// Add parameters left and right and returns the result
+// Add method that takes parameters left and right and returns the result
 func add(left: Int, right: Int) -> Int {
     return left + right
 }
 
-// Subtract parameters left and right and returns the result
+// Subtract method that takes parameters left and right and returns the result
 func subtract(left: Int, right: Int) -> Int {
         return left - right
 }
 
-// Multiply parameters left and right and returns the result
+// Multiply method that takes parameters left and right and returns the result
 func multiply(left: Int, right: Int) -> Int {
     return left * right
 }
 
-// Divide parameters left and right and returns the result
+// Divide method that takes parameters left and right and returns the result
 func divide(left: Int, right: Int) -> Int {
         return left / right
 }
@@ -34,8 +34,23 @@ func mathOperation(left: Int, right: Int, operation: (Int, Int) -> Int) -> Int {
     return operation(left, right)
 }
 
+// add method that takes a list as the parameter, add all elements and returns the result
+func add(array: Array<Int>) -> Int {
+    var result: Int = 0
+    for index in array {
+        result += index
+    }
+    return result
+}
 
-
+// multiply method that takes a list as the parameter, multiply all elements and returns the result
+func multiply(array: Array<Int>) -> Int {
+    var result: Int = 0
+    for index in array {
+        result *= index
+    }
+    return result
+}
 
 
 
