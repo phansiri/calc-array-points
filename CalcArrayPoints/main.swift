784d35be05595493417b3f58fb1eb066a38799d8
lit_phansiri <phansiri@uw.edu>
2016-10-08 15:26:18 -0700

    updated comments on basic calculator functions

--- CalcArrayPoints/main.swift
+++ CalcArrayPoints/main.swift
@@ -8,37 +8,34 @@
 
 import Foundation
 
-// Add function
+// Calculator
+// Add parameters left and right and returns the result
 func add(left: Int, right: Int) -> Int {
     return left + right
 }
 
-// Subtract function
+// Subtract parameters left and right and returns the result
 func subtract(left: Int, right: Int) -> Int {
         return left - right
 }
 
-// Multiplright function
+// Multiply parameters left and right and returns the result
 func multiply(left: Int, right: Int) -> Int {
     return left * right
 }
 
-// Divide function
-// BIG BUG FOR DIVIDING
+// Divide parameters left and right and returns the result
 func divide(left: Int, right: Int) -> Int {
         return left / right
 }
 
-//func divide(first left: Int, right right: Int) -> Double {
-//    return Double(left) / Double(right)
-//}
-
-// Basic math function
+// Math operation that takes parameters left, right and operation and returns the result
 func mathOperation(left: Int, right: Int, operation: (Int, Int) -> Int) -> Int {
     return operation(left, right)
 }
 
-//print(basicMath(first: 10, right: 100, operand: divide))
+
+
 
 
 
