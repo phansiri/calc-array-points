550e236b230ec9ad40fd019535f9b90a69ad16c5
lit_phansiri <phansiri@uw.edu>
2016-10-08 10:44:11 -0700

    Initial Commit

---
+++ CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by Litthideth Phansiri on 10/8/16.
+//  Copyright © 2016 Lit Phansiri. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
