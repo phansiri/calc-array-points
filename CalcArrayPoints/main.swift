4017f1891ebd305811d92adc149b8a6973689418
lit_phansiri <phansiri@uw.edu>
2016-10-08 15:41:34 -0700

    fully working tested array function

--- CalcArrayPoints/main.swift
+++ CalcArrayPoints/main.swift
@@ -34,6 +34,7 @@ func mathOperation(left: Int, right: Int, operation: (Int, Int) -> Int) -> Int {
     return operation(left, right)
 }
 
+// Array Fun
 // add method that takes a list as the parameter, add all elements and returns the result
 func add(array: Array<Int>) -> Int {
     var result: Int = 0
@@ -51,6 +52,26 @@ func multiply(array: Array<Int>) -> Int {
     }
     return result
 }
+
+// count method that takes a list as the parameter, and returns how many are in the list as the result
+func count(array: Array<Int>) -> Int {
+    return array.count
+}
+
+// average method that takes a list as the parameter, calcucate all elements and returns the average as the result
+func average(array: Array<Int>) -> Int {
+    var result: Int = 0
+    for index in array {
+        result += index
+    }
+    return result / array.count
+}
+
+// Generic math method that takes a list, and operation and perform the correct calculations and returns the result
+func reduce(array: Array<Int>, operation: (Array<Int>) -> Int) -> Int {
+    return operation(array)
+}
+
 
 
 
